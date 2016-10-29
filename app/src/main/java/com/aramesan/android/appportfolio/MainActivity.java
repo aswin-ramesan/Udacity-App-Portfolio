265bf02750e226e9f44f71be8ae7e5bf6179bf0e
ashz <aswinramesan@gmail.com>
2016-10-29 20:37:47 +0300

    Adding all the code to the new repository

---
+++ app/src/main/java/com/aramesan/android/appportfolio/MainActivity.java
@@ -0,0 +1,53 @@
+package com.aramesan.android.appportfolio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     *
+     * @param value
+     * @return
+     */
+    private static String upperCaseAllFirst(String value) {
+
+        char[] array = value.toCharArray();
+
+        // Uppercase first letter.
+        array[0] = Character.toUpperCase(array[0]);
+
+        // Uppercase all letters that follow a whitespace character.
+        for (int i = 1; i < array.length; i++) {
+            if (Character.isWhitespace(array[i - 1])) {
+                array[i] = Character.toUpperCase(array[i]);
+            }
+        }
+
+        return new String(array);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void displayToast(View view)
+    {
+        Button clickedButton = (Button) view;
+
+        String btnText = clickedButton.getText().toString();
+        String toastBtnText = upperCaseAllFirst(btnText.toLowerCase());
+
+        Toast toast = Toast.makeText(getApplicationContext(), "This button will launch my " + toastBtnText + " project.", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
